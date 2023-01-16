e67ca4e80ec978fde4a4f8200869c2c2e448902d
Vecher <infinity_345@mail.ru>
2023-01-16 15:08:19 +0300

    Add files

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 120;  //  Цена билета
+
+        int y = 20; // стоимость 1 бонуса
+
+        int bonus = x / y;
+
+        System.out.println("Количество бонусов: " + bonus);
+
+
+    }
+
+
+
+}
+
